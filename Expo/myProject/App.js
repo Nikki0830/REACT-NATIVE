2da3a41be48e7926606547bb88917747ffbb403c
Nikki0830 <gniki0830@gmail.com>
2024-02-12 00:00:00 +0530

    REACT_NATIVE

--- Expo/myProject/App.js
+++ Expo/myProject/App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>
+        {" "}
+        Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height:"20px",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
